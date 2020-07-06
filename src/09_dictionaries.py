e3c167e5c6ee6c0a1ed39e3efcffc162cd2afed8
cjakuc <chris.jakuc@gmail.com>
2020-07-06 15:02:54 -0400

    1-12 done

--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -35,6 +35,12 @@ waypoints = [
 
 # Add a new waypoint to the list
 # YOUR CODE HERE
+new_waypoint = {
+    "lat": 1,
+    "lon": 1,
+    "name": "new place"
+}
+waypoints.append(new_waypoint)
 
 # Modify the dictionary with name "a place" such that its longitude
 # value is -130 and change its name to "not a real place"
@@ -42,6 +48,12 @@ waypoints = [
 # waypoints list.
 
 # YOUR CODE HERE
+waypoints[0]["lon"] = -130
+waypoints[0]["name"] = "not a real place"
 
 # Write a loop that prints out all the field values for all the waypoints
 # YOUR CODE HERE
+for x in waypoints:
+    print(x['lat'])
+    print(x["lon"])
+    print(x["name"])
--- src/11_args.py
+++ src/11_args.py
@@ -5,6 +5,8 @@
 # the sum. This is what you'd consider to be a regular, normal function.
 
 # YOUR CODE HERE
+def f1(x: int, y: int):
+    return(x + y)
 
 print(f1(1, 2))
 
@@ -13,6 +15,12 @@ print(f1(1, 2))
 # Note: Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
+def f2(*nums):
+    # print(type(nums))
+    total = 0
+    for x in nums:
+        total += x
+    return total
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -22,7 +30,17 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # How do you have to modify the f2 call below to make this work?
-print(f2(a))    # Should print 22
+def f2_redo(*nums):
+    total = 0
+    if type(nums[0]) == list:
+        for x in nums[0]:
+            total += x
+    elif type(nums[0]) == int:
+        for x in nums:
+            total += x
+    return total
+
+print(f2_redo(a))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
@@ -30,6 +48,8 @@ print(f2(a))    # Should print 22
 # Note: Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
+def f3(x, y=1):
+    return(x+y)
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
@@ -44,6 +64,9 @@ print(f3(8))     # Should print 9
 # Note: Google "python keyword arguments".
 
 # YOUR CODE HERE
+def f4(a, b):
+    print(f"key: a, value: {a}")
+    print(f"key: b, value: {b}")
 
 # Should print
 # key: a, value: 12
@@ -54,7 +77,12 @@ f4(a=12, b=30)
 # key: city, value: Berkeley
 # key: population, value: 121240
 # key: founded, value: "March 23, 1868"
-f4(city="Berkeley", population=121240, founded="March 23, 1868")
+def f4_redo(city, population, founded):
+    print(f"key: city, value: {city}")
+    print(f"key: population, value: {population}")
+    print(f"key: founded, value: {founded}")
+
+f4_redo(city="Berkeley", population=121240, founded="March 23, 1868")
 
 d = {
     "monster": "goblin",
@@ -62,4 +90,8 @@ d = {
 }
 
 # How do you have to modify the f4 call below to make this work?
-f4(d)
+def f4_redo2(x: dict):
+    for z in list(x.keys()):
+        print(f"key: {z}, value: {x[z]}")
+
+f4_redo2(d)
